454a3e3304dad0a105767643f4f419c42d0aed22
ron_zhong <ronzhong2009@gmail.com>
2019-04-26 09:46:15 +0800

    Update JavaSymbolParserAndHandler.java
    symbol handling should be specified when requiring the target data.

--- src/main/java/com/ronzhong/JSPH/SymboInteface/JavaSymbolParserAndHandler.java
+++ src/main/java/com/ronzhong/JSPH/SymboInteface/JavaSymbolParserAndHandler.java
@@ -27,27 +27,37 @@ public class JavaSymbolParserAndHandler {
     private  CombinedTypeSolver typeSolver =  null;
 	private  List<SourceRoot> srcrootlist = null;
 	private  SymbolStorageStrategy outstorage = null;
+	private  SymbolFilterChain filterchain = null;
 	
 	
-	//save all the symbols into MogoDB
+	//Deprecated:save all the symbols into MogoDB
 	//The symbols in the file can be organized to be searched easily. 
 	// and they can be restored into the source file.
-	public JavaSymbolParserAndHandler(JavaSymbolRepository symRep, JavaSymboSolver symbolSolver, int handlercode) {
-//		
+	//==> seems it's too complicated to do this, that means should dive deeper into the AST tree. That's another story.
+	public JavaSymbolParserAndHandler(JavaSymbolRepository symRep, JavaSymboSolver symbolSolver) {
 		srcrootlist = symRep.getSourceRootList();
 		typeSolver = symbolSolver.getJavaParser();
-		
 	}
 
 
 	//handle the symbol after solving it, only for solved symbol
 	//only allow client to choose which handling they want for the symbol
-	public boolean getfilterOutSymbols(int SymbolType, int SymbolDecAttribute, String pattern, SymbolStorageStrategy strategy) {
+	// handlercode works for the target symbol!
+	public boolean getfilterOutSymbols(List<SymbolFilter> filterList, SymbolStorageStrategy strategy, int handlercode) {
+		filterchain.add(filterList);
 		outstorage = strategy;
+		//TODO: start to get the symbols and store into storage specified.
+		
 		return false;
 	}
 	
+	public filter getSymbolFilter(int symType, int attribute, String pattern ) {
+		SymbolFilter filter  = (new SymbolFilterFactory).createRule(symType, attribute, pattern);
+		return filter;
+	}
+	
 	public SymbolStorageStrategy getSymbolStorageStrategy(int storagetype, String storagepath) {
+		//TODO: leave the details to the factory
 		if(storagetype == 0) {
 			MogoDBSymbolStorageStrategy storage  = new MogoDBSymbolStorageStrategy(storagepath);
 			return storage;
