70eebbeb1180da8e1b29641ffc0a6625e72dcdbf
ron_zhong <ronzhong2009@gmail.com>
2019-05-01 23:23:06 +0800

    make JavaSymbol Solver simpler and save all the typeSolvers together in the final javaSymbolSolver

--- src/main/java/com/ronzhong/JSPH/SymboInteface/JavaSymboSolver.java
+++ src/main/java/com/ronzhong/JSPH/SymboInteface/JavaSymboSolver.java
@@ -1,11 +1,15 @@
 package com.ronzhong.JSPH.SymboInteface;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.client.ClientEndPoint;
+import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ClassLoaderTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JarTypeSolver;
@@ -14,65 +18,46 @@ import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeS
 public class JavaSymboSolver {
 	
 	private Logger logger = LoggerFactory.getLogger(ClientEndPoint.class);
-	private List<ClassLoaderTypeSolver> classloaderdepTypeSolver=null;
-	private CombinedTypeSolver comdepTypeSolver=null;
-	private List<JavaParserTypeSolver> javaparserdepTypeSolver=null;
-	private List<JarTypeSolver> depjarTypeSolver=null;
+	private CombinedTypeSolver comdepTypeSolver= new CombinedTypeSolver();;
+	private Collection<Object> typesolverllist = new ArrayList<Object>();
 	 
 	static public int SYMBOL_SOLVER_RESOURCE_PATH_TO_CODE = 0X0001;
 	static public int SYMBOL_SOLVER_RESOURCE_PATH_TO_JAR = 0X0002;
 		
 	public JavaSymboSolver(ClassLoader classloader){
-		this.classloaderdepTypeSolver  = new ClassLoaderTypeSolver(classloader);
-		comdepTypeSolver = new CombinedTypeSolver();
-		this.comdepTypeSolver.add(this.classloaderdepTypeSolver);
-		
+		addSolver(new ClassLoaderTypeSolver(classloader));
 	};
 	
-	private List<Object> getdifferntList(List<Object> srclist, List<Object> inputlist){
-		return null;
-	}
-	
 	public JavaSymboSolver(int pathType, String filepath) throws IOException{
-	
-		comdepTypeSolver = new CombinedTypeSolver();
-
-		
 		if( pathType == SYMBOL_SOLVER_RESOURCE_PATH_TO_CODE){
-			this.javaparserdepTypeSolver = new JavaParserTypeSolver(filepath);
-			this.comdepTypeSolver.add(this.javaparserdepTypeSolver);
+			addSolver(new JavaParserTypeSolver(filepath));
 		}else if( pathType == SYMBOL_SOLVER_RESOURCE_PATH_TO_JAR){
-			this.depjarTypeSolver = new JarTypeSolver(filepath);
-			this.comdepTypeSolver.add(this.depjarTypeSolver);
+			addSolver(new JarTypeSolver(filepath));
 		} else {
 			logger.error("with wrong type to create JavaSymbolSlover");
 		}
 	};
 	
-	public JavaSymboSolver addSymbolSolver(JavaSymboSolver symSolver) {
-		if(comdepTypeSolver == null)
-			comdepTypeSolver = new CombinedTypeSolver();
-		
-		//should specify
-		if(symSolver.classloaderdepTypeSolver != null) {
-			if(this.classloaderdepTypeSolver !=null) {
-				logger.error("There have been classloaer slover, another one will raise error");
-			} else {
-				this.classloaderdepTypeSolver  = symSolver.classloaderdepTypeSolver;
-				this.comdepTypeSolver.add(this.classloaderdepTypeSolver);
+	private void addSolver(Object solver) {
+		this.typesolverllist.add(solver);
+		this.comdepTypeSolver.add((TypeSolver) solver);
+	}
+	
+	private boolean searchSolver(Object solver) {
+		for(Object item:this.typesolverllist) {
+			if(solver == item) {
+				return true;			
 			}
 		}
+		return false;
+	}
+	
+	public JavaSymboSolver addSymbolSolver(JavaSymboSolver symSolver) {
 		
-		if(symSolver.depjarTypeSolver != null && symSolver.depjarTypeSolver != this.depjarTypeSolver)  {
-			this.comdepTypeSolver.add(symSolver.depjarTypeSolver);
+		for(Object item:symSolver.typesolverllist)
+		if(!searchSolver(item)) {
+			addSolver(item);
 		}
-		
-		if(symSolver.javaparserdepTypeSolver != null && symSolver.javaparserdepTypeSolver != this.javaparserdepTypeSolver)  {
-			this.comdepTypeSolver.add(symSolver.javaparserdepTypeSolver);
-		}
-		
-		//TODO: merge the SymbolSolver into the current instance
-		this.comdepTypeSolver.add(symSolver.getJavaParser());
 		return this;
 	}
 
