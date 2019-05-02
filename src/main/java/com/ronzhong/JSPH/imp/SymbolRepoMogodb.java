33446149d9411ea76adc29b8229ad8448d97759a
ron_zhong <ronzhong2009@gmail.com>
2019-05-02 11:28:05 +0800

    encapsulte the methods for CRUD in mongodb

--- src/main/java/com/ronzhong/JSPH/imp/SymbolRepoMogodb.java
+++ src/main/java/com/ronzhong/JSPH/imp/SymbolRepoMogodb.java
@@ -1,7 +1,13 @@
 package com.ronzhong.JSPH.imp;
 
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -9,37 +15,75 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.UpdateResult;
+import com.ronzhong.JSPH.SymboInteface.Symbol;
+import com.client.ClientEndPoint;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 
 class SymbolRepoMogodb {
+	Logger logger = LoggerFactory.getLogger(SymbolRepoMogodb.class);
 	private MongoCollection<Document> collection;
+	private MongoClient client = null;
+	private String dbName = null;
 	
 	public SymbolRepoMogodb() {
-		collection = connect( "javasymbolrepo", "default", "localhost", 27017);
+		this.collection = connect( "javasymbolrepo", "default", "localhost", 27017);
 	}
 	
 	public SymbolRepoMogodb(String dblocation) {
-		collection = connect(dblocation);
+		this.collection = connect(dblocation);
 	}
-	
 	
 	public SymbolRepoMogodb(String databaseName, String collectionName,
             String hostName, int port) {
-		collection = connect( databaseName, collectionName,hostName, port);
+		this.collection = connect( databaseName, collectionName,hostName, port);
 	}
 	
     private MongoCollection<Document> connect(String databaseName, String collectionName,
             String hostName, int port) {
     	MongoCollection<Document> tempCollection = null;
-        MongoClient client = MongoClients.create("mongodb://"+hostName+":"+ String.valueOf(port));
+        this.client = 
+        		MongoClients.create(
+        		        MongoClientSettings.builder()
+        		                .applyToClusterSettings(builder ->
+        		                        builder.hosts(Arrays.asList(new ServerAddress(hostName, port))))
+        		                .build());
+        this.dbName = databaseName;
         MongoDatabase db = client.getDatabase(databaseName);
-        tempCollection = db.getCollection(collectionName);
+        if(db != null) {
+        	tempCollection = db.getCollection(collectionName);
+        }else{
+        	logger.error("No db found named {}", databaseName);
+        }
         System.out.println(collection);
         return tempCollection;
     }
     
+    public boolean insert(Symbol sym) {
+    	return true;
+    }
+
+    public Collection<Symbol> access(Symbol filtersym) {
+    	//TODO: how to specify the conditions we want through the arguments?
+    	return null;
+    }
+
+    public boolean updateOne(Symbol filtersym) {
+    	//TODO: how to specify the conditions we want through the arguments?
+    	return true;
+    }
+
+    public boolean updateMany(Symbol filtersym, Symbol target) {
+    	//TODO: how to specify the conditions we want through the arguments?
+    	return true;
+    }
+
+    public int getTotalNumber() {
+    	return 0;
+    }
+
     private MongoCollection<Document> connect(String dblocation) {
     	MongoCollection<Document> tempCollection = null;
     	//TODO: extract the hostName and port number from dblocation string
